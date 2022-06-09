7dd77fc5a663da2e98f593c3287a3094b811102d
Sourav Bhukta <souravbhukta8@gmail.com>
2022-06-09 14:05:22 +0530

    almost complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,16 +5,16 @@ const authorController = require("../controllers/authorController")
 const booksController = require('../controllers/booksController')
 
 
-router.get('/test-me', function(req,res){
-    res.send("My first API")
-})
+router.
+
 
 // *******AUTHORS*********
-
+get('/test-me', function(req,res){
+    res.send("My first API")
+})
 router.post('/createAuthor', authorController.createAuthor)
 
 router.get("/authorData", authorController.authorData)
-
 // ********BOOKS***********
 
 router.post('/createBooks' ,booksController.createBooks)
@@ -22,27 +22,6 @@ router.post('/createBooks' ,booksController.createBooks)
 router.get('/booksData', booksController.booksData )
 
 
-//MOMENT 
-const moment = require('moment');
-router.get("/dateManipulations", function (req, res){
 
-
-
-
-    
-    // const today = moment();
-    // let x= today.add(10, "days")
-
-    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
-    // console.log(validOrNot)
-    
-    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
-    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
-
-    let x= dateB.diff(dateA, "days")
-    console.log(x)
-
-    res.send({ msg: "all good"})
-})
 
 module.exports = router;
